56dc6b9732b1b7c76188f47c3ce3626f36a05078
helloworld <sergeychernonosov21229@gmail.com>
2023-01-17 19:15:21 +0300

    Дз выполнил

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task1 {
+
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            T min = list.get(0);
+            T max = list.get(list.size() - 1);
+            minMaxConsumer.accept(min, max);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Stream<Integer> stream = Stream.of(-1,2,3,4,5);
+        findMinMax(stream, Comparator.naturalOrder(), (min, max) -> System.out.println("min: " + min + " max: " + max));
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class Task2 {
+    public static void numbersOfEven() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        Stream<Integer> st = numbers.stream();
+        Predicate<Integer> even = number -> number % 2 == 0;
+        System.out.println(st.filter(even).count());
+    }
+    public static void main(String[] args) {
+        numbersOfEven();
+    }
 }
